22f20cbd74e07c010cc3bf1eda6c50f621db71e2
Kudastech <kudastech@gmail.com>
2024-12-14 21:47:11 +0100

    modified database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Venue;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +19,25 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Kudastech',
+            'email' => 'kudastech@gmail.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true
+        ]);
+
+        User::factory()->create([
+            'name' => 'User Kudastech',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password'),
+            // 'is_admin' => true
+        ]);
+
+
+
+        $venues = Venue::factory(10)->create();
+
+        Event::factory(20)->create([
+            'venue_id' => $venues->random()->id,
         ]);
     }
 }
